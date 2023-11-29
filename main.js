71bc5998eee05a25682d51aa3c617c3d96300a1e
Loukmane Chaou <72920928+lkmnch@users.noreply.github.com>
2023-11-29 01:14:46 +0100

    Add localStorage for data persistence

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+//DOM Elements
+
 const createButton = document.getElementById("createButton")
+const deleteList = document.getElementById("deleteList")
 const overViewTable = document.getElementById("tbody")
 const createApplicationForm = document.getElementById("createApplicationForm")
 const formElements = Array.from(
@@ -10,9 +13,88 @@ const createApplicationButton = document.getElementById(
 const closeDialogButton = document.getElementById("closeDialogButton")
 const addApplicationDialog = document.getElementById("addApplicationDialog")
 
+const applicationDetailsButton = document.getElementById(
+	"applicationDetailsButton"
+)
+const applicationDetailsDialog = document.getElementById(
+	"applicationDetailsDialog"
+)
+const applicationDetailsDialogClose = document.getElementById(
+	"applicationDetailsDialogClose"
+)
+
+const applicationDetailsDialogContent = document.getElementById(
+	"applicationDetailsDialogContent"
+)
+
+////////
+
+//Data Model
+
+let applications = [
+	/* 	{
+		company: "",
+		job: "",
+		contact: "",
+		location: "",
+		jobtype: "",
+		status: "",
+		requirements: [""],
+		source: "",
+	}, */
+]
+let rowIndex = 0
+const addNewApplication = (application) => {
+	const newRow = document.createElement("tr")
+	const checkBox = document.createElement("td")
+	checkBox.innerHTML = `<input type="checkbox" name=${rowIndex} />&nbsp;`
+	newRow.appendChild(checkBox)
+
+	applications = application.map((item) => ({
+		...item,
+		[item.name]: item.value,
+	}))
+	localStorage.setItem("applications", JSON.stringify(applications))
+	applications.forEach((element) => {
+		if (Object.keys(element).toString() !== "submit") {
+			const tableData = document.createElement("td")
+			tableData.innerHTML = Object.values(element)
+			newRow.appendChild(tableData)
+		}
+	})
+
+	overViewTable.appendChild(newRow)
+	rowIndex++
+}
+
+const updateApplication = () => {}
+
+const deleteApplication = () => {
+	localStorage.removeItem("applications")
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	let tableRow = document.createElement("tr")
+	const applicationsData = JSON.parse(localStorage.getItem("applications"))
+	console.log(applicationsData)
+	if (applicationsData !== null) {
+		overViewTable.innerText = ""
+		applicationsData.forEach((item) => {
+			const tableData = document.createElement("td")
+			tableData.innerHTML = Object.values(item)
+			tableRow.appendChild(tableData)
+		})
+		overViewTable.appendChild(tableRow)
+	} else {
+		overViewTable.innerText = "List is empty create entry"
+	}
+})
+
 createButton.addEventListener("click", () => {
 	addApplicationDialog.showModal()
 })
+
+deleteList.addEventListener("click", () => deleteApplication())
 
 closeDialogButton.addEventListener("click", () => {
 	addApplicationDialog.close()
@@ -21,15 +103,21 @@ closeDialogButton.addEventListener("click", () => {
 
 createApplicationForm.addEventListener("submit", (e) => {
 	e.preventDefault()
-	const newRow = document.createElement("tr")
-	formElements.forEach((element) => {
-		if (element.type !== "submit") {
-			const tableData = document.createElement("td")
-			tableData.innerHTML = element.value
-			newRow.appendChild(tableData)
-		}
-	})
-	overViewTable.appendChild(newRow)
+
+	addNewApplication(formElements)
+
 	createApplicationForm.reset()
 	addApplicationDialog.close()
 })
+
+//show detail modal of each application
+
+applicationDetailsButton.addEventListener("click", () => {
+	applicationDetailsDialog.showModal()
+})
+
+applicationDetailsDialogClose.addEventListener("click", () => {
+	applicationDetailsDialog.close()
+})
+
+applicationDetailsDialogContent.innerHTML = formElements[1].value
